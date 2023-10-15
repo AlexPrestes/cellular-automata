61024edc69295c0762a9d836f20cd04cd2d5815e
Alex Prestes <alex.prestes@usp.br>
2023-10-15 19:26:49 -0300

    Seleção de novas cores [Amarelo, Azul, Verde, Vermelho] e combinação de cores na interação entre automatos

--- cellular_automata/game_of_life/__init__.py
+++ cellular_automata/game_of_life/__init__.py
@@ -6,16 +6,16 @@ from game_of_life import CellularAutomata
 
 @ti.data_oriented
 class App:
-    def __init__(self, width=1920, height=1080):
+    def __init__(self, width=1600, height=900):
         ti.init(arch=ti.cuda)
         self.res = (width, height)
-        self.window = ti.ui.Window('Game of Life', self.res)
+        self.window = ti.ui.Window('pygame', self.res)
         self.canvas = self.window.get_canvas()
-        self.frame = ti.Vector.field(3, ti.f32, self.res)
         self.ca = CellularAutomata(width, height)
-        self.copy_frame_to_current_state()
+        self.frame = ti.Vector.field(3, ti.f32, shape=self.res)
         self.radius_draw = 20
         self.paused = False
+        self.color_select = ti.Vector( (1,1,0), ti.f32 )
     
     def events(self):
         if self.window.get_event( ti.ui.PRESS ):
@@ -27,16 +27,26 @@ class App:
             
             if self.window.event.key in ['q', 'Q']:
                 exit()
+            
+            if self.window.event.key == ti.ui.LEFT:
+                self.color_select = (1, 1, 0)
+            
+            if self.window.event.key == ti.ui.RIGHT:
+                self.color_select = (0, 1, 0)
+            
+            if self.window.event.key == ti.ui.UP:
+                self.color_select = (1, 0, 0)
+            
+            if self.window.event.key == ti.ui.DOWN:
+                self.color_select = (0, 0, 1)
 
         if self.window.is_pressed(ti.ui.RMB):
             x, y = self.real_pos_mouse()
             self.erase(x, y)
-            self.copy_frame_to_current_state()
 
         if self.window.is_pressed(ti.ui.LMB):
             x, y = self.real_pos_mouse()
-            self.draw(x, y)
-            self.copy_frame_to_current_state()
+            self.draw(x, y, self.color_select)
         
     
     def real_pos_mouse(self):
@@ -44,50 +54,41 @@ class App:
         return int(self.res[0]*x), int(self.res[1]*y)
     
     @ti.func
-    def pos_shape(self, x: ti.i32, y: ti.i32, xc: ti.i32, yc: ti.i32) -> bool:
+    def pos_shape_square(self, x: ti.i32, y: ti.i32) -> bool:
         res = False
         if (x >= 0 and x < self.res[0]) and (y >= 0 and y < self.res[1]):
             res = True
         return res
     
+    @ti.func
+    def pos_shape_sphere(self, x: ti.i32, y: ti.i32, xc: ti.i32, yc: ti.i32) -> bool:
+        res = False
+        if (x >= 0 and x < self.res[0]) and (y >= 0 and y < self.res[1]) and ((x-xc)**2 + (y-yc)**2 <= self.radius_draw**2):
+            res = True
+        return res
+    
     @ti.kernel
-    def draw(self, x: ti.i32, y: ti.i32):
+    def draw(self, x: ti.i32, y: ti.i32, color: ti.math.vec3):
         for i in range(x-self.radius_draw, x+self.radius_draw+1):
             for j in range(y-self.radius_draw, y+self.radius_draw+1):
-                if self.pos_shape(i, j, x, y):
-                    self.frame[i, j] = (1, 1, 0)
+                if self.pos_shape_sphere(i, j, x, y) and ti.random(float) <= 0.05:
+                    self.ca.current_state[i, j] = color
     
     @ti.kernel
     def erase(self, x: ti.i32, y: ti.i32):
         for i in range(x-self.radius_draw, x+self.radius_draw+1):
             for j in range(y-self.radius_draw, y+self.radius_draw+1):
-                if self.pos_shape(i, j, x, y):
-                    self.frame[i, j] = (0, 0, 0)
-    
-    @ti.kernel
-    def copy_frame_to_current_state(self):
-        for x, y in self.frame:
-            if self.frame[x, y][0] == 1:
-                self.ca.current_state[x, y] = 1
-            else:
-                self.ca.current_state[x, y] = 0
-    
-    @ti.kernel
-    def copy_current_state_to_frame(self):
-        for x, y in self.ca.current_state:
-            if self.ca.current_state[x, y]:
-                self.frame[x, y] = (1, 1, 0)
-            else:
-                self.frame[x, y] = (0, 0, 0)
+                if self.pos_shape_sphere(i, j, x, y):
+                    self.ca.current_state[i, j] = self.ca.color_background
     
     def reset(self):
-        self.ca.current_state.from_numpy(np.zeros((self.res[0], self.res[1]), dtype=np.int32))
+        self.ca.current_state.fill(self.ca.color_background)
     
     def update(self):
         if not self.paused:
             self.ca.step()
             self.ca.current_state.copy_from(self.ca.next_state)
-        self.copy_current_state_to_frame()
+        self.frame.copy_from(self.ca.current_state)
     
 
     def draw_mouse_contour(self):
@@ -101,9 +102,9 @@ class App:
     def run(self):
         while self.window.running:
             self.events()
-            self.draw_mouse_contour()
-            self.canvas.set_image(self.frame)
             self.update()
+            #self.draw_mouse_contour()
+            self.canvas.set_image(self.ca.current_state)
             self.window.show()
 
 
--- cellular_automata/game_of_life/game_of_life.py
+++ cellular_automata/game_of_life/game_of_life.py
@@ -6,35 +6,41 @@ import taichi as ti
 class CellularAutomata:
     def __init__(self, width=1600, height=900):
         self.size = (width, height)
-        self.current_state = ti.field(ti.int32, shape=self.size)
-        self.next_state = ti.field(ti.int32, shape=self.size)
-        self.current_state.fill(0)
-        self.next_state.fill(0)
+        self.current_state = ti.Vector.field(3, ti.f32, shape=self.size)
+        self.next_state = ti.Vector.field(3, ti.f32, shape=self.size)
+        self.color_background = ti.Vector( (0, 0, 0), ti.f32)
+        self.current_state.fill( self.color_background )
+        self.next_state.fill( self.color_background )
     
     @ti.func
-    def count_neighbors(self, x, y):
-        neighbors = 0
+    def count_neighbors(self, x: ti.i32, y: ti.i32):
+        neighbors: ti.i32 = 0
+        color_means = ti.Vector( (0, 0, 0), ti.f32)
         for i in range(x-1, x+2):
             for j in range(y-1, y+2):
-                #if (i >= 0 and i < self.size[0]) and (j >= 0 and j < self.size[1]):
-                neighbors += self.current_state[i, j]
-        return neighbors
+                if any(self.current_state[i, j] != self.color_background):
+                    neighbors += 1
+                    color_means += self.current_state[i, j]
+        if neighbors != 0:
+            color_means /= neighbors
+        return neighbors, color_means
 
     @ti.func
-    def rules_cell(self, x, y):
-        neighbors = self.count_neighbors(x, y)
+    def rules_cell(self, x: ti.i32, y: ti.i32):
+        neighbors, color = self.count_neighbors(x, y)
 
-        if self.current_state[x, y] == 1:
+        if any(self.current_state[x, y] != self.color_background):
             neighbors -= 1
             if neighbors == 2 or neighbors == 3:
-                self.next_state[x, y] = 1
+                self.next_state[x, y] = color
             else:
-                self.next_state[x, y] = 0
+                self.next_state[x, y] = self.color_background
         else:
             if neighbors == 3:
-                self.next_state[x, y] = 1
+                self.next_state[x, y] = color
             else:
-                self.next_state[x, y] = 0
+                self.next_state[x, y] = self.color_background
+        
 
     @ti.kernel
     def step(self):
